d954979c5cd51444a4e89aaf09be1e1dedc28e25
Anthony Prescott-Brooks <anthonybrooks226@gmail.com>
2019-09-23 21:55:10 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -20,9 +20,7 @@ class Student
       FROM students
       WHERE grade = 9 
     SQL
-    DB[:conn].execute(sql).map do |row|
-      self.new_from_db(row)
-    end 
+    DB[:conn].execute(sql)
   end 
   
   def self.students_below_12th_grade
