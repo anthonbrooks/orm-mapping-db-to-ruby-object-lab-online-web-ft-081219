fcbfbc07f06fdbda5f0afab2e4c05259b088c7ff
Anthony Prescott-Brooks <anthonybrooks226@gmail.com>
2019-09-23 22:00:39 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -31,6 +31,23 @@ class Student
     SQL
     DB[:conn].execute(sql)
   end 
+  
+  def self.first_X_students_in_grade_10
+    
+  end 
+  
+  def self.first_student_in_grade_10 
+    sql = <<-SQL
+      SELECT * 
+      FROM students
+      WHERE grade = 10 
+      LIMIT 1 
+    SQL
+  end 
+  
+  def self.all_students_in_grade_X
+    
+  end 
 
   def self.find_by_name(name)
     sql = <<-SQL
