23495c56a2c3f488647efa93eb95485c338b9f2d
Anthony Prescott-Brooks <anthonybrooks226@gmail.com>
2019-09-23 22:16:50 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -58,7 +58,7 @@ class Student
       SELECT * 
       FROM students
       WHERE grade = 10 
-      ORDER BY student.id
+      ORDER BY students.id
       LIMIT 1 
     SQL
     DB[:conn].execute(sql).map do |row|
