654bc4ed8bf652b1956d96102cf03ba778a3d8c3
Anthony Prescott-Brooks <anthonybrooks226@gmail.com>
2019-09-23 22:14:34 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -46,10 +46,11 @@ class Student
       SELECT *
       FROM students
       WHERE grade = 10 
-      LIMIT numb
+      LIMIT #{numb}
     SQL
     DB[:conn].execute(sql).map do |row|
       self.new_from_db(row)
+    end
   end 
   
   def self.first_student_in_grade_10 
